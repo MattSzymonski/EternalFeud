d064b8ed7c626342a49eff6f971785560493e191
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2019-09-09 17:45:33 +0200

    Perpendicular Vector

---
+++ Template_MightyGamePack_HDRP/Assets/Other/MightyGamePack/Scripts/Core/MightyUtilites.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MightyGamePack
+{
+    public class MightyUtilites
+    {
+        public static Vector3 ClearY(Vector3 vector)
+        {
+            return new Vector3(vector.x, 0, vector.z);
+        }
+
+        public static Vector3 PerpendicularToLine(Vector3 lineStartPoint, Vector3 lineEndPoint)
+        {
+            Vector3 lineDir = lineStartPoint - lineEndPoint;
+            Vector3 perpendicularDir = Vector3.Cross(lineDir, Vector3.up).normalized;
+            return perpendicularDir;
+        }
+    }
+}
---
+++ Template_MightyGamePack_HDRP/Assets/Scripts/PerpendicularDir.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MightyGamePack;
+
+public class PerpendicularDir : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform point1;
+    public Transform point2;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Debug.DrawLine(point1.position, point2.position);
+        
+
+
+        
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        //Vector3 middlePoint = Vector3.Lerp(point1.position, point2.position, 0.5f);
+
+        Vector3 middlePoint = point1.position - (point1.position - point2.position) * 0.5f;
+
+        Gizmos.DrawLine(point1.position, point2.position);
+        Gizmos.DrawSphere(MightyUtilites.ClearY(middlePoint), 0.5f);
+        Gizmos.DrawLine(MightyUtilites.ClearY(middlePoint), MightyUtilites.ClearY(middlePoint) + MightyUtilites.PerpendicularToLine(point1.position, point2.position) * 10);
+    }
+}
